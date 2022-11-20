1a499f4faf32e1662ac14649d59d9fb1f67db3db
Alisa Grabko <grabkoalisa@gmail.com>
2022-11-20 16:49:32 +0200

    new changes

--- src/main/java/lesson_one/Main.java
+++ src/main/java/lesson_one/Main.java
@@ -3,5 +3,8 @@ package lesson_one;
 public class Main {
     public static void main(String[] args){
 System.out.println("Hello World");
+        System.out.println("QA automation");
+        System.out.println("our work with git");
+        System.out.println("Hello World");
     }
 }
